5e17370d8572431047d2bff51ef46e9e9589f07c
ziadhany-77 <113359289+ziadhany-77@users.noreply.github.com>
2026-01-09 16:44:59 +0200

    Enhance stored procedures with validation checks
    Added validation checks for inserting and updating departments and tracks, ensuring unique names and valid department IDs.

--- Scripts/Stored Procedures/select-insert-update-delete/SQLQuery_FirstScript_TrackAndDepartmentSPs.sql
+++ Scripts/Stored Procedures/select-insert-update-delete/SQLQuery_FirstScript_TrackAndDepartmentSPs.sql
@@ -6,7 +6,21 @@ use ITI_ExamSystem
 -- insertion into Department
 create proc SP_Department_Insert @Department_Name varchar(20)
 as
-	insert into Department values (@Department_Name)
+	IF @Department_Name IS NULL OR LTRIM(RTRIM(@Department_Name)) = ''
+	BEGIN
+		RAISERROR('Department cant be empty.', 16, 1);
+		return;
+    END
+	IF EXISTS (
+        SELECT 1
+        FROM Department
+        WHERE LOWER(DeptName) = LOWER(@Department_Name)
+    )
+    BEGIN
+        RAISERROR('Department already exists.', 16, 1);
+        RETURN;
+    END
+	insert into Department (DeptName) values (@Department_Name)
 
 
 
@@ -24,8 +38,17 @@ as
 
 
 --updating department name
-create proc SP_Department_Update @Department_id int, @New_Name varchar(20)
+alter proc SP_Department_Update @Department_id int, @New_Name varchar(20)
 as
+	IF EXISTS (
+        SELECT 1
+        FROM Department
+        WHERE LOWER(DeptName) = LOWER(@New_Name)
+    )
+    BEGIN
+        RAISERROR('Department already exists.', 16, 1);
+        RETURN;
+    END
 	update Department set DeptName = @New_Name
 	where DeptID = @Department_id
 
@@ -40,16 +63,29 @@ as
 
 
 
-
+	-----------------------------------------------------------------------
 --insertion in track
 alter proc SP_Track_Insert @Track_Name varchar(20), @Department_ID int
 as
-	if  NOT EXISTS (select 1 from Department where DeptID = @Department_ID)
-	BEGIN
-        select 'there is no Existing department with id' + convert(varchar,@Department_ID)
-		return;
+	IF EXISTS (
+        SELECT 1
+        FROM Track
+        WHERE LOWER(TrackName) = LOWER(@Track_Name)
+    )
+    BEGIN
+        RAISERROR('Track already exists.', 16, 1);
+        RETURN;
     END
-	insert into Track values (@Track_Name, @Department_ID)
+	IF NOT EXISTS (
+        SELECT 1
+        FROM Department
+        WHERE DeptID = @Department_ID
+    )
+    BEGIN
+        RAISERROR('Invalid Department id', 16, 1);
+        RETURN;
+    END
+	insert into Track (TrackName, DeptID) values (@Track_Name, @Department_ID)
 
 
 --select from track
@@ -66,14 +102,53 @@ as
 
 
 --Update in track
-create proc SP_Track_Update @Track_Name varchar(20), @Track_ID int
+alter proc SP_Track_Update @Track_New_Name varchar(20), @Track_ID int
 as
-	Update Track set TrackName = @Track_Name
+	IF EXISTS (
+        SELECT 1
+        FROM Track
+        WHERE LOWER(TrackName) = LOWER(@Track_New_Name)
+    )
+    BEGIN
+        RAISERROR('this Track already exists.', 16, 1);
+        RETURN;
+    END
+	Update Track set TrackName = @Track_New_Name
 	where TrackID = @Track_ID
 
---Deletion from Track
-create proc SP_Track_Delete @Track_ID int
+
+
+
+--Move Track to another Department
+create proc SP_Track_UpdateDept @Track_ID int, @New_Department_ID int
 as
+	IF NOT EXISTS (
+        SELECT 1
+        FROM Department
+        WHERE DeptID = @New_Department_ID
+    )
+    BEGIN
+        RAISERROR('Invalid Department id', 16, 1);
+        RETURN;
+    END
+	Update Track set DeptID = @New_Department_ID
+	where TrackID = @Track_ID
+
+
+
+
+--Deletion from Track
+alter proc SP_Track_Delete @Track_ID int
+as
+    IF NOT EXISTS (
+        SELECT 1
+        FROM Track
+        WHERE TrackID = @Track_ID
+    )
+    BEGIN
+        RAISERROR('Invalid Track id', 16, 1);
+        RETURN;
+    END
 	delete from Track where TrackID = @Track_ID
 
 
@@ -82,16 +157,17 @@ as
 
 
 SP_Department_selectAll
-SP_Department_selectByID 3
-SP_Department_Insert 'Game Development'
-SP_Department_Update 2, 'dept updated name'
-SP_Department_Delete 2
+SP_Department_selectByID 5
+SP_Department_Insert 'Java'
+SP_Department_Update 5, 'New department change'
+SP_Department_Delete 5
 
 
-SP_Track_Insert 'pd', 1
+SP_Track_Insert 'new new track', 4
 SP_track_SelectAll
-SP_track_SelectByID 4
-SP_Track_Update 'newTrackName', 4
-SP_Track_Delete 4
+SP_track_SelectByID 6
+SP_Track_Update 'PD', 6
+SP_Track_UpdateDept 6, 1
+SP_Track_Delete 7
 select * from Department
 select * from Track
