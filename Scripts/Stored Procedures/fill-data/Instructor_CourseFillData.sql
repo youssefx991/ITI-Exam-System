9f8d5ad4dab8186d1ef3c5fbfa9173df3584ae74
Youssef Wagih <youssefwagih212@gmail.com>
2026-01-12 22:31:38 +0200

    fill instructor_course with new data

--- Scripts/Stored Procedures/fill-data/Instructor_CourseFillData.sql
+++ Scripts/Stored Procedures/fill-data/Instructor_CourseFillData.sql
@@ -1,7 +1,6 @@
 ﻿USE ITI_ExamSystem;
 GO
 
-
 ------------------------------------------------
 -- Each instructor teaches 2 courses
 ------------------------------------------------
@@ -36,7 +35,77 @@ EXEC sp_Instructor_Course_Insert 9, 18;
 EXEC sp_Instructor_Course_Insert 10, 19;
 EXEC sp_Instructor_Course_Insert 10, 20;
 
+EXEC sp_Instructor_Course_Insert 11, 21;
+EXEC sp_Instructor_Course_Insert 11, 22;
 
+EXEC sp_Instructor_Course_Insert 12, 23;
+EXEC sp_Instructor_Course_Insert 12, 24;
 
+EXEC sp_Instructor_Course_Insert 13, 25;
+EXEC sp_Instructor_Course_Insert 13, 26;
 
+EXEC sp_Instructor_Course_Insert 14, 27;
+EXEC sp_Instructor_Course_Insert 14, 28;
+
+EXEC sp_Instructor_Course_Insert 15, 29;
+EXEC sp_Instructor_Course_Insert 15, 30;
+
+EXEC sp_Instructor_Course_Insert 16, 31;
+EXEC sp_Instructor_Course_Insert 16, 32;
+
+EXEC sp_Instructor_Course_Insert 17, 33;
+EXEC sp_Instructor_Course_Insert 17, 34;
+
+EXEC sp_Instructor_Course_Insert 18, 35;
+EXEC sp_Instructor_Course_Insert 18, 36;
+
+EXEC sp_Instructor_Course_Insert 19, 37;
+EXEC sp_Instructor_Course_Insert 19, 38;
+
+EXEC sp_Instructor_Course_Insert 20, 39;
+EXEC sp_Instructor_Course_Insert 20, 40;
+
+EXEC sp_Instructor_Course_Insert 21, 41;
+EXEC sp_Instructor_Course_Insert 21, 42;
+
+EXEC sp_Instructor_Course_Insert 22, 43;
+EXEC sp_Instructor_Course_Insert 22, 44;
+
+EXEC sp_Instructor_Course_Insert 23, 45;
+EXEC sp_Instructor_Course_Insert 23, 46;
+
+EXEC sp_Instructor_Course_Insert 24, 47;
+EXEC sp_Instructor_Course_Insert 24, 48;
+
+EXEC sp_Instructor_Course_Insert 25, 49;
+EXEC sp_Instructor_Course_Insert 25, 50;
+
+EXEC sp_Instructor_Course_Insert 26, 51;
+EXEC sp_Instructor_Course_Insert 26, 52;
+
+EXEC sp_Instructor_Course_Insert 27, 53;
+EXEC sp_Instructor_Course_Insert 27, 54;
+
+EXEC sp_Instructor_Course_Insert 28, 55;
+EXEC sp_Instructor_Course_Insert 28, 56;
+
+EXEC sp_Instructor_Course_Insert 29, 57;
+EXEC sp_Instructor_Course_Insert 29, 58;
+
+EXEC sp_Instructor_Course_Insert 30, 59;
+EXEC sp_Instructor_Course_Insert 30, 60;
+
+EXEC sp_Instructor_Course_Insert 31, 61;
+EXEC sp_Instructor_Course_Insert 31, 62;
+
+EXEC sp_Instructor_Course_Insert 32, 63;
+EXEC sp_Instructor_Course_Insert 32, 64;
+
+EXEC sp_Instructor_Course_Insert 33, 65;
+
+------------------------------------------------
+-- Verify
+------------------------------------------------
+EXEC sp_Instructor_Course_SelectAll;
+GO
 
