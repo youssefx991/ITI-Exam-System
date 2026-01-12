e9435cedf68944967d5446da71eedfe821be6bf5
Youssef Wagih <youssefwagih212@gmail.com>
2026-01-12 22:06:10 +0200

    put correct ids in instructor_course fill data

--- Scripts/Stored Procedures/fill-data/Instructor_CourseFillData.sql
+++ Scripts/Stored Procedures/fill-data/Instructor_CourseFillData.sql
@@ -1,72 +1,42 @@
 ﻿USE ITI_ExamSystem;
+GO
 
--- TrackID 10 – Data Science → InstructorID 11, 12
-EXEC sp_Instructor_Course_Insert 11, 67;
-EXEC sp_Instructor_Course_Insert 11, 68;
-EXEC sp_Instructor_Course_Insert 11, 69;
-EXEC sp_Instructor_Course_Insert 11, 70;
-EXEC sp_Instructor_Course_Insert 11, 71;
 
-EXEC sp_Instructor_Course_Insert 12, 67;
-EXEC sp_Instructor_Course_Insert 12, 68;
-EXEC sp_Instructor_Course_Insert 12, 69;
-EXEC sp_Instructor_Course_Insert 12, 70;
-EXEC sp_Instructor_Course_Insert 12, 71;
+------------------------------------------------
+-- Each instructor teaches 2 courses
+------------------------------------------------
 
--- TrackID 16 – IoT Systems → InstructorID 13
-EXEC sp_Instructor_Course_Insert 13, 72;
-EXEC sp_Instructor_Course_Insert 13, 73;
-EXEC sp_Instructor_Course_Insert 13, 74;
-EXEC sp_Instructor_Course_Insert 13, 75;
-EXEC sp_Instructor_Course_Insert 13, 76;
+EXEC sp_Instructor_Course_Insert 1, 1;
+EXEC sp_Instructor_Course_Insert 1, 2;
 
--- TrackID 4 – Professional Web Dev → InstructorID 14
-EXEC sp_Instructor_Course_Insert 14, 77;
-EXEC sp_Instructor_Course_Insert 14, 78;
-EXEC sp_Instructor_Course_Insert 14, 79;
-EXEC sp_Instructor_Course_Insert 14, 80;
-EXEC sp_Instructor_Course_Insert 14, 81;
+EXEC sp_Instructor_Course_Insert 2, 3;
+EXEC sp_Instructor_Course_Insert 2, 4;
 
--- TrackID 7 – Business Intelligence → InstructorID 15
-EXEC sp_Instructor_Course_Insert 15, 82;
-EXEC sp_Instructor_Course_Insert 15, 83;
-EXEC sp_Instructor_Course_Insert 15, 84;
-EXEC sp_Instructor_Course_Insert 15, 85;
-EXEC sp_Instructor_Course_Insert 15, 86;
+EXEC sp_Instructor_Course_Insert 3, 5;
+EXEC sp_Instructor_Course_Insert 3, 6;
 
--- TrackID 8 – ERP and Enterprise A → InstructorID 16
-EXEC sp_Instructor_Course_Insert 16, 87;
-EXEC sp_Instructor_Course_Insert 16, 88;
-EXEC sp_Instructor_Course_Insert 16, 89;
-EXEC sp_Instructor_Course_Insert 16, 90;
-EXEC sp_Instructor_Course_Insert 16, 91;
+EXEC sp_Instructor_Course_Insert 4, 7;
+EXEC sp_Instructor_Course_Insert 4, 8;
 
--- TrackID 12 – Artificial Intelligence → InstructorID 17
-EXEC sp_Instructor_Course_Insert 17, 97;
-EXEC sp_Instructor_Course_Insert 17, 98;
-EXEC sp_Instructor_Course_Insert 17, 99;
-EXEC sp_Instructor_Course_Insert 17, 100;
-EXEC sp_Instructor_Course_Insert 17, 101;
+EXEC sp_Instructor_Course_Insert 5, 9;
+EXEC sp_Instructor_Course_Insert 5, 10;
 
--- TrackID 13 – Cybersecurity → InstructorID 18
-EXEC sp_Instructor_Course_Insert 18, 102;
-EXEC sp_Instructor_Course_Insert 18, 103;
-EXEC sp_Instructor_Course_Insert 18, 104;
-EXEC sp_Instructor_Course_Insert 18, 105;
-EXEC sp_Instructor_Course_Insert 18, 106;
+EXEC sp_Instructor_Course_Insert 6, 11;
+EXEC sp_Instructor_Course_Insert 6, 12;
 
--- TrackID 6 – Software Testing → InstructorID 19
-EXEC sp_Instructor_Course_Insert 19, 112;
-EXEC sp_Instructor_Course_Insert 19, 113;
-EXEC sp_Instructor_Course_Insert 19, 114;
-EXEC sp_Instructor_Course_Insert 19, 115;
-EXEC sp_Instructor_Course_Insert 19, 116;
+EXEC sp_Instructor_Course_Insert 7, 13;
+EXEC sp_Instructor_Course_Insert 7, 14;
 
--- TrackID 15 – Embedded Systems Dev → InstructorID 20
-EXEC sp_Instructor_Course_Insert 20, 122;
-EXEC sp_Instructor_Course_Insert 20, 123;
-EXEC sp_Instructor_Course_Insert 20, 124;
-EXEC sp_Instructor_Course_Insert 20, 125;
-EXEC sp_Instructor_Course_Insert 20, 126;
+EXEC sp_Instructor_Course_Insert 8, 15;
+EXEC sp_Instructor_Course_Insert 8, 16;
 
-EXEC sp_Instructor_Course_SelectAll
+EXEC sp_Instructor_Course_Insert 9, 17;
+EXEC sp_Instructor_Course_Insert 9, 18;
+
+EXEC sp_Instructor_Course_Insert 10, 19;
+EXEC sp_Instructor_Course_Insert 10, 20;
+
+
+
+
+
